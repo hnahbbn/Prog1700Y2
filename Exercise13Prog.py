036a00ee2ec762bb4b401210b13de040158517af
hannah <hannahgbs@gmail.com>
2024-10-30 22:08:41 -0300

    have two options/columns, "name" and "age"

--- Exercise13Prog.py
+++ Exercise13Prog.py
@@ -2,3 +2,4 @@ import csv
 def collect_party_guests(file_path="guests.csv"):
     with open(file_path, mode="w", newline='') as file:
         writer = csv.writer(file)
+        writer.writerow(["Name", "Age"])
